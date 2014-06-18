aafcf48873dc2a91e073f0e3a2d7ee385ffca281
Ryan Bigg <radarlistener@gmail.com>
2014-06-18 14:09:17 +1000

    Add mega-comment for Order#check_promotions

--- lib/spree/order.rb
+++ lib/spree/order.rb
@@ -22,6 +22,44 @@ module Spree
       sum_adjustments(line_items.map(&:adjustments).flatten)
     end
 
+    # In order to get an accurate adjustment total, we must check all the individual line items
+    # and their respective promotions. By the time #update_totals is called, another line item 
+    # may have been added. If this happens, promotions which were once ineligible may become
+    # eligible. This method makes those promotions eligible.
+    #
+    # An example:
+    #
+    # Order #1
+    #   - Line Item #1 - $20
+    #
+    # Promotion #1
+    #   - Type: Cart
+    #   - Rule: Item total > $10
+    #   - Action: $2.50 off each item
+    #
+    # Promotion #2
+    #    - Type: Cart
+    #   - Rule: Item total > $30
+    #   - Action: $5 off each line item.
+    #
+    # At this point, with both promotions having already been activated on the order, only
+    # Promotion #1 should be eligible, given that the order total is only $20.
+    #
+    # When a new item is added, this will most-certainly effect the final total. Let's say
+    # another item of $20 is added to this order, making the total $40. The
+    # PromotionHandler::Cart class will declare Promotion #2 the winner instantly for this
+    # new item. The original item's promotions are never checked to see if their eligibility
+    # changes, so therefore the adjustments look like this:
+    #
+    # Line Item #1
+    #   - Adjustment (Promotion #1): $2.50 ELIGIBLE
+    #   - Adjustment (Promotion #2): $5.00 INELIGIBLE
+    # Line Item #2
+    #   - Adjustment (Promotion #1): $2.50 INELIGIBLE
+    #   - Adjustment (Promotion #2): $5.00 ELIGIBLE
+    #
+    # This method is designed to prevent cases like this by quickly recomputing each line
+    # item's promotion total once more.
     def check_promotions
       line_items.each do |item|
         Spree::ItemAdjustments.new(item).calculate_promo_total
