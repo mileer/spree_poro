c950b1bec8f2b5c07adf2f4f1e64485ce112ff03
Ryan Bigg <radarlistener@gmail.com>
2014-06-18 14:23:57 +1000

    Remove unnecessary to_f call in store_pre_tax_amount

--- lib/spree/tax_rate.rb
+++ lib/spree/tax_rate.rb
@@ -24,7 +24,7 @@ module Spree
     # https://github.com/spree/spree/issues/4318#issuecomment-34723428
     def self.store_pre_tax_amount(item, rates)
       if rates.any?(&:included_in_price)
-        rate_total = rates.map(&:amount).inject(&:+).to_f
+        rate_total = rates.map(&:amount).inject(&:+)
         item.pre_tax_amount = item.discounted_amount / (1 + rate_total)
       end
     end
